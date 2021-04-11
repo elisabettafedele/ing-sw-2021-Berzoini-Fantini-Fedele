d4858ba2210d7f85aa8a1f31812126892257ef44
elisabettafedele <65794629+elisabettafedele@users.noreply.github.com>
2021-04-11 10:33:56 +0200

    json files for cards added and Value constructor updated

--- src/main/java/it/polimi/ingsw/model/cards/Value.java
+++ src/main/java/it/polimi/ingsw/model/cards/Value.java
@@ -2,11 +2,7 @@ package it.polimi.ingsw.model.cards;
 
 import it.polimi.ingsw.enumerations.Resource;
 import it.polimi.ingsw.exceptions.InvalidArgumentException;
-import it.polimi.ingsw.model.cards.DevelopmentCard;
-import it.polimi.ingsw.model.cards.Flag;
-import it.polimi.ingsw.model.cards.LeaderCard;
 
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -14,7 +10,7 @@ import java.util.Map;
  */
 public class Value {
 
-    private List<Flag> flagValue;
+    private Map<Flag, Integer> flagValue;
     private Map<Resource, Integer> resourceValue;
     private int faithValue;
 
@@ -25,101 +21,16 @@ public class Value {
      * @param faithValue the quantity of Faith Points
      * @throws InvalidArgumentException
      */
-    public Value(List<Flag> flagValue, Map<Resource, Integer> resourceValue, int faithValue) throws InvalidArgumentException {
-        if (faithValue < 0 || flagValue == null || resourceValue == null){
+    public Value(Map<Flag, Integer> flagValue, Map<Resource, Integer> resourceValue, int faithValue) throws InvalidArgumentException {
+        if (faithValue < 0 ){
             throw new InvalidArgumentException();
         }
-        this.flagValue = flagValue;
-        this.resourceValue = resourceValue;
-        this.faithValue = faithValue;
-    }
-    /**
-     * Constructs a value associated with a cost or a production power.
-     * @param flagValue the {@link Flag} of the activation cost
-     * @param resourceValue the type and quantity of the {@link Resource}
-     * @throws InvalidArgumentException
-     */
-    public Value(List<Flag> flagValue, Map<Resource, Integer> resourceValue) throws InvalidArgumentException {
-        if (flagValue == null || resourceValue == null){
-            throw new InvalidArgumentException();
-        }
-        this.flagValue = flagValue;
-        this.resourceValue = resourceValue;
-        this.faithValue = 0;
-    }
-
-    /**
-     * Constructs a value associated with a cost or a production power.
-     * @param flagValue the {@link Flag} of the activation cost
-     * @param faithValue the quantity of Faith Points
-     * @throws InvalidArgumentException
-     */
-    public Value(List<Flag> flagValue, int faithValue) throws InvalidArgumentException {
-        if (faithValue < 0 || flagValue == null){
-            throw new InvalidArgumentException();
-        }
-        this.flagValue = flagValue;
-        this.resourceValue = null;
-        this.faithValue = faithValue;
-    }
-    /**
-     * Constructs a value associated with a cost or a production power.
-     * @param resourceValue the type and quantity of the {@link Resource}
-     * @param faithValue the quantity of Faith Points
-     * @throws InvalidArgumentException
-     */
-    public Value(Map<Resource, Integer> resourceValue, int faithValue) throws InvalidArgumentException {
-        if (faithValue < 0 || resourceValue == null){
-            throw new InvalidArgumentException();
-        }
-        this.flagValue = null;
-        this.resourceValue = resourceValue;
+        this.flagValue = (flagValue == null || flagValue.isEmpty()) ? null : flagValue;
+        this.resourceValue = (resourceValue == null || resourceValue.isEmpty()) ? null : resourceValue;
         this.faithValue = faithValue;
     }
 
-    /**
-     * Constructs a value associated with a cost or a production power.
-     * @param flagValue the {@link Flag} of the activation cost
-     * @throws InvalidArgumentException
-     */
-    public Value(List<Flag> flagValue) throws InvalidArgumentException {
-        if (flagValue == null){
-            throw new InvalidArgumentException();
-        }
-        this.flagValue = flagValue;
-        this.resourceValue = null;
-        this.faithValue = 0;
-    }
-
-    /**
-     * Constructs a value associated with a cost or a production power.
-     * @param resourceValue the type and quantity of the {@link Resource}
-     * @throws InvalidArgumentException
-     */
-    public Value(Map<Resource, Integer> resourceValue) throws InvalidArgumentException {
-        if(resourceValue==null){
-            throw new InvalidArgumentException();
-        }
-        this.resourceValue = resourceValue;
-        this.flagValue = null;
-        this.faithValue = 0;
-    }
-
-    /**
-     * Constructs a value associated with a cost or a production power.
-     * @param faithValue the quantity of Faith Points
-     * @throws InvalidArgumentException
-     */
-    public Value(int faithValue) throws InvalidArgumentException {
-        if (faithValue < 0){
-            throw new InvalidArgumentException();
-        }
-        this.faithValue = faithValue;
-        this.flagValue = null;
-        this.resourceValue = null;
-    }
-
-    public List<Flag> getFlagValue() {
+    public Map<Flag, Integer> getFlagValue() {
         return flagValue;
     }
 
--- src/test/java/it/polimi/ingsw/model/cards/ValueTest.java
+++ src/test/java/it/polimi/ingsw/model/cards/ValueTest.java
@@ -2,29 +2,25 @@ package it.polimi.ingsw.model.cards;
 
 import it.polimi.ingsw.enumerations.Resource;
 import it.polimi.ingsw.exceptions.InvalidArgumentException;
-import it.polimi.ingsw.model.cards.Flag;
-import it.polimi.ingsw.model.cards.Value;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class ValueTest {
 
     Value value;
-    List<Flag> flagValue;
+    Map<Flag, Integer> flagValue;
     Map<Resource, Integer> resourceValue;
     int faithValue;
 
     @Before
     public void setUp() throws Exception {
-        flagValue = new ArrayList<>();
+        flagValue = new HashMap<>();
         resourceValue = new HashMap<>();
         faithValue = 5;
         value = new Value(flagValue, resourceValue, faithValue);
@@ -54,95 +50,7 @@ public class ValueTest {
         Value invalidValue = new Value(null, null, -1);
     }
 
-    @Test
-    public void Value_constructor_OnlyResourceValue() throws InvalidArgumentException {
-        Value v = new Value(resourceValue);
-    }
-
-    @Test
-    public void Value_constructor_OnlyFaithValue() throws InvalidArgumentException {
-        Value v = new Value(faithValue);
-    }
-
-    @Test
-    public void Value_constructor_OnlyFlagValue() throws InvalidArgumentException {
-        Value v = new Value(flagValue);
-    }
-
-    @Test (expected = InvalidArgumentException.class)
-    public void Value_constructor_InvalidArgumentException_OnlyNegativeFaithValue() throws InvalidArgumentException {
-        Value invalidValue = new Value(-1);
-    }
-
-    @Test (expected = InvalidArgumentException.class)
-    public void Value_constructor_InvalidArgumentException_OnlyFlagValueNull() throws InvalidArgumentException {
-        List<Flag> inv = null;
-        Value invalidValue = new Value(inv);
-    }
-
-    @Test (expected = InvalidArgumentException.class)
-    public void Value_constructor_InvalidArgumentException_OnlyResourceValueNull() throws InvalidArgumentException {
-        Map<Resource, Integer> inv = null;
-        Value invalidValue = new Value(inv);
-    }
-
-    @Test (expected = InvalidArgumentException.class)
-    public void Value_constructor_InvalidArgumentException_2argsResourceNull() throws InvalidArgumentException {
-        Map<Resource, Integer> inv = null;
-        Value invalidValue = new Value(inv, 5);
-    }
-
-    @Test (expected = InvalidArgumentException.class)
-    public void Value_constructor_InvalidArgumentException_2argsFaithNull() throws InvalidArgumentException {
-        Map<Resource, Integer> inv = new HashMap<>();
-        Value invalidValue = new Value(inv, -5);
-    }
-
-    @Test
-    public void Value_constructor_InvalidArgumentException_2argsNotNull() throws InvalidArgumentException {
-        Map<Resource, Integer> r = new HashMap<>();
-        Value value1 = new Value(r, 5);
-        assertNull(value1.getFlagValue());
-    }
-
-    @Test (expected = InvalidArgumentException.class)
-    public void Value_constructor_InvalidArgumentException_2argsFlagNull() throws InvalidArgumentException {
-        List<Flag> inv = null;
-        Map<Resource, Integer> r = null;
-        Value invalidValue = new Value(inv, r);
-    }
-
-    @Test (expected = InvalidArgumentException.class)
-    public void Value_constructor_InvalidArgumentException_2argsResourcesNull() throws InvalidArgumentException {
-        List<Flag> inv = new ArrayList<>();
-        Map<Resource, Integer> r = null;
-        Value invalidValue = new Value(inv, r);
-    }
-
-    @Test
-    public void Value_constructor_InvalidArgumentException_2argsResourcesFlagNotNull() throws InvalidArgumentException {
-        List<Flag> inv = new ArrayList<>();
-        Map<Resource, Integer> r = new HashMap<>();
-        Value invalidValue = new Value(inv, r);
-    }
-
-    @Test (expected = InvalidArgumentException.class)
-    public void Value_constructor_InvalidArgumentException_2argsFlagValueNull() throws InvalidArgumentException {
-        List<Flag> inv = null;
-        Value invalidValue = new Value(inv, 5);
-    }
-
-    @Test (expected = InvalidArgumentException.class)
-    public void Value_constructor_InvalidArgumentException_2argsFaithNegative() throws InvalidArgumentException {
-        List<Flag> inv = new ArrayList<>();
-        Value invalidValue = new Value(inv, -5);
-    }
-
-    @Test
-    public void Value_constructor_InvalidArgumentException_2argsFaithFlagNotNull() throws InvalidArgumentException {
-        List<Flag> inv = new ArrayList<>();
-        Value invalidValue = new Value(inv, 5);
-    }
+    
 
 
 }
