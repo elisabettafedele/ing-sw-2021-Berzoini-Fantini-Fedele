12b14739b02adab0be9d224c3b58d280b6f3872c
elisabettafedele <elisabettafedele99@gmail.com>
2021-06-28 00:35:56 +0200

    Solves bug in check faith track last round

--- src/main/java/it/polimi/ingsw/controller/game_phases/MultiplayerEndPhase.java
+++ src/main/java/it/polimi/ingsw/controller/game_phases/MultiplayerEndPhase.java
@@ -2,7 +2,6 @@ package it.polimi.ingsw.controller.game_phases;
 
 import it.polimi.ingsw.messages.toClient.game.GameOverMessage;
 import it.polimi.ingsw.model.player.Player;
-
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -24,11 +23,11 @@ public class MultiplayerEndPhase extends EndPhase {
      * @return a {@link GameOverMessage} which contains all the results
      */
     public GameOverMessage getEndMessage(boolean readyForAnotherGame){
-        List<Player> sortedPlayers = getController().getPlayers().stream().sorted(Comparator.comparingInt(Player::getVictoryPoints).reversed()).collect(Collectors.toList());
+        List<Player> sortedPlayers = getController().getPlayers().stream().sorted(Comparator.comparingInt(Player::countPoints).reversed()).collect(Collectors.toList());
         Map<String, Integer> results = new LinkedHashMap<>();
 
         for (Player player : sortedPlayers){
-            results.put(player.getNickname(), player.getVictoryPoints());
+            results.put(player.getNickname(), player.countPoints());
         }
         return new GameOverMessage(results, readyForAnotherGame);
     }
--- src/main/java/it/polimi/ingsw/controller/game_phases/SinglePlayerEndPhase.java
+++ src/main/java/it/polimi/ingsw/controller/game_phases/SinglePlayerEndPhase.java
@@ -5,7 +5,7 @@ import it.polimi.ingsw.messages.toClient.game.NotifyPointsSinglePlayer;
 public class SinglePlayerEndPhase extends EndPhase {
     @Override
     public void notifyResults() {
-        getController().getConnectionByNickname(getController().getPlayers().get(0).getNickname()).sendMessageToClient(new NotifyPointsSinglePlayer(getController().getPlayers().get(0).isWinner() ? getController().getPlayers().get(0).getVictoryPoints() : -1));
+        getController().getConnectionByNickname(getController().getPlayers().get(0).getNickname()).sendMessageToClient(new NotifyPointsSinglePlayer(getController().getPlayers().get(0).isWinner() ? getController().getPlayers().get(0).countPoints() : -1));
         getController().getServer().gameEnded(getController());
     }
 }
